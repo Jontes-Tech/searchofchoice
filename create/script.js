31ca1da6e7ba3583bed6ad720f0eb6d4ef1b1aed
jonte <jonatan@jontes.page>
2022-06-04 10:28:18 +0200

    Bunch of updates

--- create/script.js
+++ create/script.js
@@ -1,4 +1,36 @@
-function submit() {
-navigator.clipboard.writeText('https://searchofchoice.jontes.page/?q=' + encodeURIComponent(document.getElementById('textbox').value));
-alert("Copied To Your Clipboard");
+function submit()   {
+    console.log("Test");
+    switch (document.getElementById('domain').value) {
+        case 'jontespage':
+            longbranded();
+            break;
+        case 'short':
+            shorturl();
+            break;
+        case 'vercelapp':
+            longunbranded();
+            break;
+        default:
+            longbranded();
+            break;
+    }
+}
+function longbranded() {
+    navigator.clipboard.writeText('https://searchofchoice.jontes.page/?q=' + encodeURIComponent(document.getElementById('textbox').value));
+    alert("Copied Long URL to Your Clipboard");
+}
+function longunbranded() {
+    navigator.clipboard.writeText('https://searchofchoice.vercel.app/?q=' + encodeURIComponent(document.getElementById('textbox').value));
+    alert("Copied Long URL to Your Clipboard");
+}
+function shorturl() {
+    const Http = new XMLHttpRequest();
+    const url='https://api.jontes.page/short-soc?q=' + encodeURIComponent(document.getElementById('textbox').value);
+    Http.open("GET", url);
+    Http.send();
+    
+    Http.onreadystatechange = (e) => {
+        navigator.clipboard.writeText(Http.responseText);
+    }
+    alert("Copied Short URL to Your Clipboard");
 }
