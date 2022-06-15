3e927ae84ffce3342723e8eb4624883f3f1f8ead
jonte <jonatan@jontes.page>
2022-06-15 18:55:36 +0200

    Removing domain-select.

--- create/script.js
+++ create/script.js
@@ -3,57 +3,6 @@ function Response(text) {
     change.innerHTML = text;
 }
 function submit()   {
-    console.log("Test");
-    switch (document.getElementById('domain').value) {
-        case 'jontespage':
-            longbranded();
-            break;
-        case 'short':
-            shorturl();
-            break;
-        case 'vercelapp':
-            longunbranded();
-            break;
-        case 'tiny':
-            tinyurl()
-        default:
-            tinyurl();
-            break;
-    }
-}
-function longbranded() {
-    navigator.clipboard.writeText('https://searchofchoice.jontes.page/?q=' + encodeURIComponent(document.getElementById('textbox').value));
-    Response("Copied Long URL to Your Clipboard");
-}
-function longunbranded() {
-    navigator.clipboard.writeText('https://searchofchoice.vercel.app/?q=' + encodeURIComponent(document.getElementById('textbox').value));
-    Response("Copied Long URL to Your Clipboard");
-}
-function shorturl() {
-    const Http = new XMLHttpRequest();
-    const url='https://api.jontes.page/short-soc?q=' + encodeURIComponent(document.getElementById('textbox').value);
-    Http.open("GET", url);
-    Http.send();
-    
-    Http.onreadystatechange = (e) => {
-        navigator.clipboard.writeText(Http.responseText);
-    }
-    Response("Copied Short URL to Your Clipboard");
-}
-
-function tinyurl() {
-    RegisterURL()
-}
-
-for (const button of document.querySelectorAll('button')) {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        submit();
-    });
-}
-
-
-function RegisterURL() {
     axios.post('https://soc.jontes.page/add', {
         term: encodeURIComponent(document.getElementById('textbox').value)
     })
@@ -65,4 +14,11 @@ function RegisterURL() {
     .catch(function (error) {
         console.log(error);
     })
+}
+
+for (const button of document.querySelectorAll('button')) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        submit();
+    });
 }
