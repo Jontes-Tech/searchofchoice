ae846c5c151dd12aece3c4b85b1c3e9ed9b82e7a
jonte <jonatan@jontes.page>
2022-08-21 12:12:39 +0200

    css fixes

--- src/lib/MainPage.svelte
+++ src/lib/MainPage.svelte
@@ -27,23 +27,15 @@
   </div>
 
 <style>
-  * {
-    font-family: "Poppins", sans-serif;
-    color: #fff;
-  }
   .search-container {
     max-width: 600px;
     margin: 50px auto;
-  }
-  button {
-    margin: 10px
   }
   input {
     display: block;
     width: calc(100% - 24px);
     font-size: 18px;
     font-weight: 600;
-    color: #e8e8e8;
     padding: 10px;
     border-radius: 10px;
     border: 2px solid #4b00ff;
