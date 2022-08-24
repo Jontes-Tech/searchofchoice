6210b58ff1ff09462880a3bdd48d81efe61c5d8f
jonte <jonatan@jontes.page>
2022-08-24 07:21:37 +0200

    TOS

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import MainPage from './lib/MainPage.svelte';
     import EngineSelect from './lib/EngineSelect.svelte';
+    import TOS from './lib/TOS.svelte'
 
     let [, page] = new URL(location.href).pathname.split('/');
 </script>
@@ -8,6 +9,8 @@
 <main>
     {#if page === 'engineselect' || !localStorage.getItem('engine')}
         <EngineSelect />
+    {:else if page === 'tos'}
+        <TOS />
     {:else}
         <MainPage />
     {/if}
@@ -16,7 +19,7 @@
         <p>
             SOC is licensed under the GPL version 3 or later.<br /><a
                 href="/engineselect/">Change your search engine preferences</a
-            >
+            ><br /><a href="/tos">Terms of Service</a>
         </p>
     </footer>
 </main>
---
+++ src/lib/TOS.svelte
@@ -0,0 +1,7 @@
+<h1>📖 Terms of Service 📖</h1>
+<h2>§ 1. Terms and definitions</h2>
+<p>§ 1.1. <b>Our Domain names</b>, often called "Our Domains", are, but not limited to SearchOfChoice.jontes.page, SearchOfChoice.vercel.app and l.og.ax.</p>
+<p>§ 1.2. <b>Users</b>, often called "The User", are considered people or animals using SearchOfChoice to visit a "SearchOfChoice hyperlink", a hyperlink using <b>Our Domain Names</b>, or create one, short or long Search Of Choice hyperlink of ones own.</p>
+<h2>§ 2. Use common sense</h2>
+<p>§ 2.1. You, a <b>User</b>, are not allowed to create a Search Of Choice hyperlink, pointing to one of <b>Our Domain Names</b> if they include terms illegal in Sweden, or your country of residence.</p>
+<p>§ 2.2. You shalt not put me in trouble.</p>
