ef4afdd70cc8e0994138742c2101654820d1d201
jonte <jonatan@jontes.page>
2022-08-26 20:04:34 +0200

    feat(src/lib/CustomEngineModal.svelte): create component

---
+++ src/lib/CustomEngineModal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let close: () => void;
+    export let setEngine: (engine: string) => void;
+
+    let engineURL = '';
+</script>
+
+<h3>Enter a custom engine URL</h3>
+<input
+    type="text"
+    placeholder="https://example.org/?q="
+    bind:value={engineURL}
+/>
+<div class="btns">
+    <button on:click={close}> Cancel </button>
+    <button class="submit-btn" on:click={() => setEngine(engineURL)}
+        > Submit </button
+    >
+</div>
+
+<style>
+    .btns {
+        margin-top: 0.1em;
+    }
+    .submit-btn {
+        background-color: #4b00ff;
+    }
+</style>
